a7dd574a4f7a1eb8d9e298fbcddc8d2a540421e5
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-09-12 00:46:04 -0400

    config: adjust connection to MongoDb and enable cache.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'src/config/.env' }),
-    MongooseModule.forRoot(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/config/.env',
+      cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URI') ??
+          `mongodb://${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DB')}`,
+        retryWrites: true,
+        w: 'majority',
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
   ],
 })
 export class AppModule {}
