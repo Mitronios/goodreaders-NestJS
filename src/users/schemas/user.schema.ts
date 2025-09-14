f1b1fbec9561872d50b4d8ec9d2a2a23ce9b89be
Jorge Villar <jorge.vfg@gmail.com>
2025-09-14 11:01:49 +0200

    Initial commit: requirments for login - model, userdto, JWT dependencies

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -9,6 +9,9 @@ export class User extends Document {
   @Prop({ required: true, unique: true, lowercase: true, trim: true })
   email: string;
 
+  @Prop({required: true})
+  password: string
+
   @Prop({ required: false, trim: true, maxlength: 500 })
   bio?: string;
 
