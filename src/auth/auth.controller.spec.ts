fa1cf2e6aea1c332cfb60d466e49e0dd2de955a6
Jorge Villar <jorge.vfg@gmail.com>
2025-09-15 21:06:39 +0200

    Added auth.controller tests, jest.config.json file and route for testin in package.json

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { describe } from 'node:test';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
+
+  const mockAuthService = {
+    validateUser: jest.fn(),
+    login: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should call AuthService.validateUser and AuthService.login', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: '123456',
+      };
+
+      const mockUser = { _id: 'userId1', email: loginDto.email, role: 'user' };
+      const mockToken = { access_token: 'jwtSimulatedToken' };
+
+      mockAuthService.validateUser.mockResolvedValue(mockUser);
+      mockAuthService.login.mockReturnValue(mockToken);
+
+      const result = await controller.login(loginDto);
+
+      expect(mockAuthService.validateUser).toHaveBeenCalledWith(
+        loginDto.email,
+        loginDto.password,
+      );
+      expect(mockAuthService.login).toHaveBeenCalledWith(mockUser);
+      expect(result).toEqual(mockToken);
+    });
+
+    it('should throw error if validateUser fails', async () => {
+      const loginDto: LoginDto = {
+        email: 'wrong@example.com',
+        password: 'wrongpass',
+      };
+
+      mockAuthService.validateUser.mockRejectedValue(
+        new Error('Invalid credentials'),
+      );
+
+      await expect(controller.login(loginDto)).rejects.toThrow(
+        'Invalid credentials',
+      );
+    });
+  });
+
+  describe('logout', () => {
+    it('should return logout message', async () => {
+      const result = await controller.logout();
+      expect(result).toEqual({ message: 'Logout successful' });
+    });
   });
 });
