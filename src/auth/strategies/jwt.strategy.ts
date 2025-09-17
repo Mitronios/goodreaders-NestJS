b5db03de1319eb68ad16dd01c900c66538d8af34
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-09-16 20:31:10 -0400

    chore: apply format where needed.

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,9 +9,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: 'utilizarenv', //SUSTITUIR POR ENV
-    })
+    });
   }
   async validate(payload: any) {
-    return {userId: payload.sub, email: payload.email, role: payload.role}
+    return { userId: payload.sub, email: payload.email, role: payload.role };
   }
 }
