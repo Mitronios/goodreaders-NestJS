9de31689084defb80435b7cfbb7cc2f084d9618d
Jorge Villar <jorge.vfg@gmail.com>
2025-09-18 20:17:10 +0200

    Format

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { AuthModule } from './auth/auth.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
